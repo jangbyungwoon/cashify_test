f35cfca1554fc2fc9070f90a524d8e0d2801dc85
btsoft <btsoft@DESKTOP-FPESDDD>
2025-03-19 14:51:40 +0900

    수정

--- cashify-test/src/main/java/kr/bts/cashify/CashifyTestApplication.java
+++ cashify-test/src/main/java/kr/bts/cashify/CashifyTestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CashifyTestApplication {
-
+	// 수정
 	public static void main(String[] args) {		
 		SpringApplication.run(CashifyTestApplication.class, args);
 	}

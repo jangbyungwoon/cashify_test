d66e631a8ddd0b5eb0b2baceab2ea564e06911a5
btsoft <btsoft@DESKTOP-FPESDDD>
2025-03-20 11:04:24 +0900

    [1]

--- src/main/java/kr/bts/cashify/CashifyTestApplication.java
+++ src/main/java/kr/bts/cashify/CashifyTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CashifyTestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("헬로 월드!");
 		SpringApplication.run(CashifyTestApplication.class, args);
 	}
 

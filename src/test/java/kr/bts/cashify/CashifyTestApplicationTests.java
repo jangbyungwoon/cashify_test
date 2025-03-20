669dcc814cbba5b0c8a597f551de72419de3e476
btsoft <btsoft@DESKTOP-FPESDDD>
2025-03-20 10:20:33 +0900

    커밋

---
+++ src/test/java/kr/bts/cashify/CashifyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.bts.cashify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashifyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
